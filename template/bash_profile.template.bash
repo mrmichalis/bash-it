1cbcd83f40343355319b0addb335cb2f82426031
mrmichalis <mihaliz@gmail.com>
2013-04-03 15:21:49 +0100

    added Hadoop env

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -32,5 +32,35 @@ export TODO="t"
 # https://github.com/xvzf/vcprompt
 #export VCPROMPT_EXECUTABLE=~/.vcprompt/bin/vcprompt
 
+# - - - - - - - <HADOOP>
+# Java Home
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+# Tools directory
+export TOOLS=$HOME/Work/tools
+
+# Installations directory
+export INSTALLS=$HOME/Work/installs
+
+# CDH Apache Hadoop and Ecosystem
+export HADOOP_PREFIX=$INSTALLS/hadoop-2.0.0
+export HADOOP_MR1_HOME=$INSTALLS/mr1-2.0.0
+export HADOOP_LOG_DIR=$HADOOP_PREFIX/logs
+export HADOOP_CONF_DIR=$HADOOP_PREFIX/etc/hadoop
+#export HADOOP_MAPRED_HOME=$HADOOP_PREFIX/share/hadoop/mapreduce
+#export HADOOP_YARN_HOME=$HADOOP_PREFIX/share/hadoop/mapreduce
+#export HADOOP_COMMON_HOME=$HADOOP_PREFIX/share/hadoop/common
+#export HADOOP_HDFS_HOME=$HADOOP_PREFIX/share/hadoop/hdfs
+export OOZIE_HOME=$INSTALLS/oozie-3.3.0
+export OOZIE_URL=http://localhost:11000/oozie
+export PIG_HOME=$INSTALLS/pig-0.10.0
+export HIVE_HOME=$INSTALLS/hive-0.10.0
+export HIVE_CONF_DIR=$HIVE_HOME/conf
+export HBASE_HOME=$INSTALLS/hbase-0.94.2
+export ZOOKEEPER_HOME=$INSTALLS/zookeeper-3.4.5
+
+export PATH=$HADOOP_PREFIX/bin:$HADOOP_PREFIX/sbin:$OOZIE_HOME/bin:$PIG_HOME/bin:$HIVE_HOME/bin:$HBASE_HOME/bin:$ZOOKEEPER_HOME/bin:$PATH
+# - - - - - - - </HADOOP >
+
 # Load Bash It
 source $BASH_IT/bash_it.sh
